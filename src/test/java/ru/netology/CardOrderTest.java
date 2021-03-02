9c1fa78491d90c2b22654ee82c342163eb991ca9
Anastasia Baturina <89099934219@mail.ru>
2021-03-03 00:02:30 +0300

    initial commit

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,64 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+    @Test
+    void shouldSubmitValidation() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+
+    @Test
+    void shouldNotSubmitWithWrongName() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Ivanov Ivan");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotSubmitWithWrongPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+799994");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+
+
+    @Test
+    void shouldNotSubmitWithWrongNameAndPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Ivanov Ivan");
+        $("[data-test-id=phone] input").setValue("89999999999");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotSubmitWithoutAgreement() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("button").click();
+        $("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
+    }
+
+
+}
