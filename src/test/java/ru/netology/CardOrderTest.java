ef4e43f1ed2dd9c1782aff217c68d3a349aa9c4b
Anastasia Baturina <89099934219@mail.ru>
2021-03-06 17:20:52 +0300

    in c

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -15,7 +15,9 @@ public class CardOrderTest {
         $("[data-test-id=phone] input").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]")
+                .shouldHave(exactText("Ваша заявка успешно отправлена! " +
+                        "Наш менеджер свяжется с вами в ближайшее время."));
     }
 
 
@@ -26,7 +28,9 @@ public class CardOrderTest {
         $("[data-test-id=phone] input").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name] .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. " +
+                        "Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -36,9 +40,9 @@ public class CardOrderTest {
         $("[data-test-id=phone] input").setValue("+799994");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
-
 
 
     @Test
@@ -48,7 +52,9 @@ public class CardOrderTest {
         $("[data-test-id=phone] input").setValue("89999999999");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name] .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. " +
+                        "Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -60,5 +66,26 @@ public class CardOrderTest {
         $("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
     }
 
+    @Test
+    void shouldEmptyName() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=name] .input__sub")
+                .shouldHave(exactText("Поле обязательно для заполнения"));
 
+    }
+
+    @Test
+    void shouldEmptyNumber() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=phone] .input__sub")
+                .shouldHave(exactText("Поле обязательно для заполнения"));
+    }
 }
